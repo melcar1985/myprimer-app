cccd3aa3332b35a83d941bb21e902a71633f70a7
melcar1985 <melisacarabajal9@gmail.com>
2022-07-11 18:23:16 -0300

    Corrección de segunda entrega

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,8 +2,25 @@ import React from "react";
 
 export const NavBar =()=>{
    return (
-   <h1>Mi NavBar</h1> 
-   );
+      
+        <div className="container">
+        <nav className='nav'>
+          <div className='nav_brand'>
+          <h1>FLOR DEL SOL</h1>
+           <div>
+            <img src="" alt="" />
+           </div>
+          </div>
+          <ul className='nav__list'>
+            <li><a className='nav__link' href='#'>PLANTAS INTERIOR</a></li>
+            <li><a className='nav__link' href='#'>PLANTAS EXTERIOR</a></li>
+            <li><a className='nav__link' href='#'>MACETAS</a></li>
+            <li><a className='nav__link' href='#'>REGALÁ</a></li>
+            <li><a className='nav__link' href='#'>CARRITO</a></li>
+            </ul>
+            </nav>
+        </div>
+        );
    
    
 }
