4ccd44034a603fcdc2d4f7648fb1081e95b68854
melcar1985 <melisacarabajal9@gmail.com>
2022-07-17 01:41:45 -0300

    Agrego contador con boton

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import Title from '../Title';
+import ItemCount from '../ItemCount';
 
 export const ItemListContainer = ({texto}) => {
+
+    const onAdd = (quantity) =>{
+        console.log(`Compraste ${quantity} unidades`);
+    }
+
     return(
+        <>
         <Title greeting={texto} />
+        <ItemCount initial={1} stock={10} onAdd={onAdd}/>
+        </>
+        
     );
 
 }
