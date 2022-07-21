30cbc8493fac20e781928d7f051385c5d3b8092e
melcar1985 <melisacarabajal9@gmail.com>
2022-07-20 23:54:00 -0300

    Entrega 5

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
-import Title from '../Title';
 import ItemCount from '../ItemCount';
+import ItemList from '../ItemList';
+import React, {useState, useEffect} from 'react';
+import Title from '../Title';
+
+
+
+const films ={
+    id: 1, Image:"https://cdn.pixabay.com/photo/2012/03/01/00/55/garden-19830_960_720.jpg"
+}
 
 export const ItemListContainer = ({texto}) => {
+
+   const [data, setData] = useState([]);
+
+   useEffect(() =>{
+     const getData = new Promise(resolve =>{
+       setTimeout(() =>{
+        resolve (films);
+       }, 3000);
+    });
+    getData.then(res => setData(res)); 
+
+
+   }, [])
+
+   
 
     const onAdd = (quantity) =>{
         console.log(`Compraste ${quantity} unidades`);
@@ -12,6 +34,7 @@ export const ItemListContainer = ({texto}) => {
         <>
         <Title greeting={texto} />
         <ItemCount initial={1} stock={10} onAdd={onAdd}/>
+        <ItemList data={data}/>
         </>
         
     );
